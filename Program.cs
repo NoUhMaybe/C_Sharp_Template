f93ed3a62af1ab9b62b98e43e2dfc3788c2044e0
nouhmaybe <nouhmaybe@NOAHSOPTIPLEX>
2025-02-06 09:21:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProjectTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MethodTemplate();
+
+            Console.ReadKey();
+
+        }
+
+        static void MethodTemplate()
+        {
+            Console.WriteLine("Hello AGAIN World!");
+        }
+    }
+}
+
+
+
