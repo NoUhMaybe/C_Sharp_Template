8368b765e8706195c3cb38bfbc1e0e67b4b28d44
NoUhMaybe <168222196+NoUhMaybe@users.noreply.github.com>
2025-02-06 12:53:56 -0600

    Added notes to porperty variables in the class template

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace ProjectTemplate
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -14,14 +15,23 @@ namespace ProjectTemplate
 
         }
 
+
         static void MethodTemplate()
         {
             Console.WriteLine("Hello AGAIN World!");
         }
 
+
         class ClassTemplate
         {
-            public int Num { get; set; }
+            public int Num1 { get; set; }
+            public int Num2 { get; init; } //Can only be set inside an object initializer after construction
+            public int Num3 { get; private set; } //Can only be set inside the class methods
+
+            public int Sum()
+            {
+                return Num1 + Num2 + Num3;
+            }
 
         }
     }
